e3632e8d22e0ddd730162d26e90d524e70dfac0d
Hossein Molazem <mh.sina@gmail.com>
2016-12-19 18:11:15 +0100

    upgrade surfire plugin
    Signed-off-by:Hossein Molazem <mh.sina@gmail.com>

--- CloudServiceRecommender/src/test/java/se/kth/kandy/ejb/algorithm/MaxProfitInstanceEstimatorTest.java
+++ CloudServiceRecommender/src/test/java/se/kth/kandy/ejb/algorithm/MaxProfitInstanceEstimatorTest.java
@@ -17,90 +17,94 @@ import se.kth.kandy.ejb.factory.EjbFactory;
 public class MaxProfitInstanceEstimatorTest {
 
   private static final Logger logger = Logger.getLogger(MaxProfitInstanceEstimatorTest.class);
-  private MaxProfitInstanceEstimator minCostInstanceEstimator = null;
+  private MaxProfitInstanceEstimator maxProfitInstanceEstimator = null;
 
   @BeforeClass
   public void setUpClass() {
-    minCostInstanceEstimator = EjbFactory.getInstance().getMaxProfitInstanceEstimator();
+    maxProfitInstanceEstimator = EjbFactory.getInstance().getMaxProfitInstanceEstimator();
   }
 
   //------------------------------------------------------------------
   @Test
   public void testEstimateSpotReliability() {
-    float reliability = minCostInstanceEstimator.estimateSpotReliability("r3.xlarge", "us-west-2c",
+    float reliability = maxProfitInstanceEstimator.estimateSpotReliability("r3.xlarge", "us-west-2c",
         new BigDecimal(0.15).setScale(4, RoundingMode.HALF_UP), 108000000L);
   }
 
   @Test
   public void testEstimateSpotReliability1() {
-    float reliability = minCostInstanceEstimator.estimateSpotReliability("d2.2xlarge", "us-east-1d",
+    float reliability = maxProfitInstanceEstimator.estimateSpotReliability("d2.2xlarge", "us-east-1d",
         new BigDecimal(0.08).setScale(4, RoundingMode.HALF_UP), 229727000L);
   }
 
   //-------------------------------------------------------------------
   @Test
   public void testEstimateTerminationAverageRunTime() {
-    double avgTime = minCostInstanceEstimator.estimateTerminationAverageRunTime("r3.xlarge", "us-west-2c",
+    double avgTime = maxProfitInstanceEstimator.estimateTerminationAverageRunTime("r3.xlarge", "us-west-2c",
         new BigDecimal(0.15).setScale(4, RoundingMode.HALF_UP), 108000000L);
   }
 
   //-------------------------------------------------------------------
   @Test
   public void testEstimateMinBid3() throws ServiceRecommanderException {
-    BigDecimal bid = minCostInstanceEstimator.estimateMinBid("d2.8xlarge", "eu-central-1b", 229727000L, (float) 0.7);
+    BigDecimal bid = maxProfitInstanceEstimator.estimateMinBid("d2.8xlarge", "eu-central-1b", 229727000L, (float) 0.7);
   }
 
   @Test
   public void testEstimateMinBid2() throws ServiceRecommanderException {
-    BigDecimal bid = minCostInstanceEstimator.estimateMinBid("d2.2xlarge", "us-east-1d", 229727000L, (float) 0.7);
+    BigDecimal bid = maxProfitInstanceEstimator.estimateMinBid("d2.2xlarge", "us-east-1d", 229727000L, (float) 0.7);
   }
 
   @Test
   public void testEstimateMinBid1() throws ServiceRecommanderException {
-    BigDecimal bid = minCostInstanceEstimator.estimateMinBid("r3.xlarge", "us-west-2c", 108000000L, (float) 0.7);
+    BigDecimal bid = maxProfitInstanceEstimator.estimateMinBid("r3.xlarge", "us-west-2c", 108000000L, (float) 0.7);
   }
 
   @Test
   public void testEstimateMinBid() throws ServiceRecommanderException {
-    BigDecimal bid = minCostInstanceEstimator.estimateMinBid("g2.2xlarge", "us-west-1a", 43200000L, (float) 0.8);
+    BigDecimal bid = maxProfitInstanceEstimator.estimateMinBid("g2.2xlarge", "us-west-1a", 43200000L, (float) 0.8);
   }
 
   //---------------------------------------------------------------------
   @Test
   public void testEstimateInstanceCost() throws ServiceRecommanderException {
-    BigDecimal cost = minCostInstanceEstimator.estimateInstanceProfit("r3.xlarge", "us-west-2c",
+    BigDecimal cost = maxProfitInstanceEstimator.estimateInstanceProfit("r3.xlarge", "us-west-2c",
         new BigDecimal(0.15).setScale(4, RoundingMode.HALF_UP), 108000000L);
   }
 
   @Test
   public void testEstimateInstanceCost1() throws ServiceRecommanderException {
-    BigDecimal cost = minCostInstanceEstimator.estimateInstanceProfit("d2.2xlarge", "eu-central-1b",
+    BigDecimal cost = maxProfitInstanceEstimator.estimateInstanceProfit("d2.2xlarge", "eu-central-1b",
         new BigDecimal(0.3).setScale(4, RoundingMode.HALF_UP), 229727000L);
   }
 
   //------------------------------------------------------------------------
   @Test
   void testFindAllInstancesZonesCost() throws Exception {
-    List<Ec2Instance> instances = minCostInstanceEstimator.findAllInstancesZonesEstimatedProfit(229727000L, (float) 0.7,
+    List<Ec2Instance> instances = maxProfitInstanceEstimator.findAllInstancesZonesEstimatedProfit(229727000L,
+        (float) 0.7,
         InstanceFilter.ECU.FIXED26, 2f, InstanceFilter.STORAGEGB.HDD12000);
   }
 
   @Test
   void testFindAllInstancesZonesCost1() throws Exception {
-    List<Ec2Instance> instances = minCostInstanceEstimator.findAllInstancesZonesEstimatedProfit(229727000L, (float) 0.7,
+    List<Ec2Instance> instances = maxProfitInstanceEstimator.findAllInstancesZonesEstimatedProfit(229727000L,
+        (float) 0.7,
         InstanceFilter.ECU.FIXED116, 244f, InstanceFilter.STORAGEGB.HDD12000);
   }
 
   @Test
   void testFindAllInstancesZonesCost2() throws Exception {
-    List<Ec2Instance> instances = minCostInstanceEstimator.findAllInstancesZonesEstimatedProfit(229727000L, (float) 0.7,
+    List<Ec2Instance> instances = maxProfitInstanceEstimator.findAllInstancesZonesEstimatedProfit(229727000L,
+        (float) 0.7,
         InstanceFilter.ECU.ALL, 0f, InstanceFilter.STORAGEGB.ALL);
   }
 
   //---------------------------------------------------------------------------
   @Test
   void testFindInstanceZonesCost() throws ServiceRecommanderException {
-    List<Ec2Instance> instances = minCostInstanceEstimator.findInstanceZonesEstimatedProfit(229727000L, (float) 0.7, "c4.8xlarge");
+    List<Ec2Instance> instances = maxProfitInstanceEstimator.findInstanceZonesEstimatedProfit(229727000L, (float) 0.7,
+        "c4.8xlarge");
   }
 
   //---------------------------------------------------------------------------
